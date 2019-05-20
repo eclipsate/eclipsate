556f2e96701706e26b460c5b8fce7923107faea1
eclipsate <paigemcallister@aol.com>
2019-05-19 21:14:19 -0400

    added midterm project

---
+++ midterm/js/sketch.js
@@ -0,0 +1,36 @@
+const BACKGROUND_COLOR = `#000`;
+const WWIDTH = 600;
+const HHEIGHT = 300;
+let input, question;
+
+function setup() {
+    var cnv = createCanvas(WWIDTH, HHEIGHT);
+    cnv.parent(`p5-sketch`);
+    background(BACKGROUND_COLOR);
+    fill(255);
+    input = createInput();
+    input.position(WWIDTH/1.3, HHEIGHT/1.95);
+
+    question = createElement(`h2`, `what is your sexuality?`);
+    question.position(WWIDTH/2.8, HHEIGHT/2.2);
+
+    textAlign(CENTER);
+    textSize(50);
+}
+
+function collage() {
+    const sexuality = input.value();
+    input.value('');
+    for (let i = 0; i < 50; i++) {
+      push();
+      fill(random(255), random(255), random(255));
+      translate(random(width), random(height));
+      rotate(random(3 * PI));
+      text(sexuality, 0, 0);
+      pop();
+  }
+}
+
+function draw() {
+    collage();
+}
---
+++ midterm/js/sketch2.js
@@ -0,0 +1,25 @@
+const BACKGROUND_COLOR = `#000`;
+const WWIDTH = 750;
+const HHEIGHT = 475;
+let capture;
+
+function setup() {
+    var cnv = createCanvas(WWIDTH, HHEIGHT);
+    cnv.parent(`p5-sketch2`);
+    fill(BACKGROUND_COLOR);
+    capture = createCapture(VIDEO);
+    capture.size(375, 237);
+}
+
+function draw() {
+      capture.position(mouseX, mouseY);
+      image(capture, 0, 0, 750, 475);
+      filter(INVERT);
+      image(capture, 0, 0, 250, 250);
+      filter(`GRAY`);
+      //end the sketch
+      if (keyIsPressed === true) {
+          noCanvas();
+          capture.position(0, 0);
+      }
+}
